25d6d10f7160bd9c061a0f6400cb534c424bc414
Philipp Reiner <phil@q-fx.de>
2020-12-11 22:12:53 +0100

    Blogtest

--- pages/index.js
+++ pages/index.js
@@ -10,17 +10,17 @@ export default function Home() {
         <title>Philipp Reiner</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Container maxWidth="54em" maxW="xl">
-        <Box pt={["32px","48px","72px","86px"]}>
+      <Container maxWidth="53em" maxW="xl">
+        <Box pt={["32px","48px","72px","86px"]} >
           <Image
             borderRadius="12px"
-            boxSize={["72px","128px","144px"]}
+            boxSize={["72px","128px","160px"]}
             src="static/philipp-reiner.jpg"
             alt="Philipp Reiner"
           />
           <Box pt="24px">
             <Heading fontWeight="500" as="h1" size="lg">
-            <Text as="span" color="primary">Philipp Reiner</Text> ist Experte für neue Geschäftsmodelle und Innovation. Ich kombiniere Strategie, User Experience und Software und ermögliche dadurch die digitale Zukunft.
+            <Text as="span" color="primary">Philipp Reiner</Text> ist Experte für neue, innovative Geschäftsmodelle. Ich kombiniere Strategie, User Experience und Software um die digitale Zukunft zu gestalten.
             </Heading>
           </Box>
           <Box pt="24px">
@@ -40,33 +40,27 @@ export default function Home() {
             </Stack>
           </Box>
         </Box>
-      
+
       </Container>
-      <Container maxWidth="58em" maxW="xl">
-          <Grid  pt="24px" templateColumns={["1fr",null,"1fr 1fr"]} gap="24px">
-            <GridItem colSpan={[1,null,2]}  boxShadow="lg" minHeight={[null,"200px","400px"]} color="white" p="24px" bg="linear-gradient(to right, #b92b27, #1565c0)" borderRadius="4px">
-              Content
-            </GridItem>
-            <GridItem boxShadow="lg"  minHeight={[null,"200px","300px"]} color="white" p="24px" borderRadius="4px">
-              Content
-            </GridItem>
-            <GridItem boxShadow="lg" minHeight={[null,"200px","300px"]} color="white" p="24px" bg="linear-gradient(to right, #0f2027, #203a43, #2c5364)" borderRadius="4px">
-              Content
-            </GridItem>
-          </Grid>
-        </Container>
-        <Box pt="24px">
-          
-             <NextLink href="/posts/first-post" passHref>
+      <Container 
+ 
+      mt={[4,8]}
+      p={[4,4,12]} 
+      pt={[4,4,8]}
+      borderRadius={[0,0,"8px"]} 
+      maxWidth="57em" 
+      maxW="xl" 
+      background="gray.50"
+      >
+      <Heading as="h2" pb="4" fontWeight="500" color="gray.500"  size="md">
+        Artikel
+      </Heading>
+      <NextLink href="/posts/first-post" passHref>
                <Link>
-                <Box bg="gray.50" p="10px" borderRadius="6px" mb="12px">
                   Das ist eine Box
-                </Box>
                 </Link>
             </NextLink>
-       
-        </Box>
-
+      </Container>
     </div>
   )
 }
