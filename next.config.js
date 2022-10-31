5c0f46a5b85c0d52bf781f506e516850ec8308b0
Philipp Reiner <phil@q-fx.de>
2022-10-31 16:25:44 +0100

    Testing different url setups

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ module.exports = {
                 destination: 'https://count.reiner.info/tracker.js'
             },
             {
-                source: '/api', 
+                source: 'ackee/api', 
                 destination: 'https://count.reiner.info/api'
             }
         ];
