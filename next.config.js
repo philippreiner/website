6832fab2afcb4a94c960637efd7b49459e29726f
Philipp Reiner <phil@q-fx.de>
2022-12-11 12:27:50 +0100

    Removed plausible

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@ module.exports = {
     async rewrites() {
         return [
             {
-                source: '/js/p.js',
-                destination: 'https://plausible.io/js/plausible.js'
-            },
-            {
-                source: '/api/event', // Or '/api/event/' if you have `trailingSlash: true` in this config
-                destination: 'https://plausible.io/api/event'
-            },
-            {
                 source: '/js/t.js', 
                 destination: 'https://count.reiner.info/tracker.js'
             },
